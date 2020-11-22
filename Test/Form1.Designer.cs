842fdd8b1fcea1c84f13e50a94457e3f3c6064ca
Daniel McGivney <daniel_mcgivney@valpak.com>
2020-11-22 10:05:46 -0500

    Add project files.

---
+++ Test/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ctlAlarmClock1 = new ctlClockLib.ctlAlarmClock();
+            this.lblTest = new System.Windows.Forms.Label();
+            this.dtpTest = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // ctlAlarmClock1
+            // 
+            this.ctlAlarmClock1.AlarmSet = false;
+            this.ctlAlarmClock1.AlarmTime = new System.DateTime(((long)(0)));
+            this.ctlAlarmClock1.ClockBlockColor = System.Drawing.Color.Empty;
+            this.ctlAlarmClock1.ClockForClor = System.Drawing.Color.Empty;
+            this.ctlAlarmClock1.Location = new System.Drawing.Point(12, 3);
+            this.ctlAlarmClock1.Name = "ctlAlarmClock1";
+            this.ctlAlarmClock1.Size = new System.Drawing.Size(800, 450);
+            this.ctlAlarmClock1.TabIndex = 0;
+            // 
+            // lblTest
+            // 
+            this.lblTest.AutoSize = true;
+            this.lblTest.Location = new System.Drawing.Point(69, 171);
+            this.lblTest.Name = "lblTest";
+            this.lblTest.Size = new System.Drawing.Size(0, 20);
+            this.lblTest.TabIndex = 1;
+            // 
+            // dtpTest
+            // 
+            this.dtpTest.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dtpTest.Location = new System.Drawing.Point(73, 216);
+            this.dtpTest.Name = "dtpTest";
+            this.dtpTest.Size = new System.Drawing.Size(164, 26);
+            this.dtpTest.TabIndex = 2;
+            this.dtpTest.ValueChanged += new System.EventHandler(this.dtpTest_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dtpTest);
+            this.Controls.Add(this.lblTest);
+            this.Controls.Add(this.ctlAlarmClock1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ctlClockLib.ctlAlarmClock ctlAlarmClock1;
+        private System.Windows.Forms.Label lblTest;
+        private System.Windows.Forms.DateTimePicker dtpTest;
+    }
+}
+
---
+++ ctlClockLib/ctlClock.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ctlClockLib
+{
+    public partial class ctlClock : UserControl
+    {
+
+        private Color colFColor;
+        private Color colBColor;
+
+        public Color ClockBlockColor
+        {
+            get { return colBColor; }
+
+            set
+            {
+                colBColor = value;
+                lblDisplay.BackColor = colBColor;
+            }
+        }
+
+
+
+        public Color ClockForClor
+        {
+            get { return colFColor; }
+
+            set
+            {
+                colFColor = value;
+                lblDisplay.ForeColor = colFColor;
+            }
+
+        }
+        public
+    ctlClock()
+        {
+            InitializeComponent();
+        }
+
+
+
+        protected virtual void timer1_Tick(object sender, EventArgs e)
+        {
+            // Causes the label to display the current time.
+            lblDisplay.Text = DateTime.Now.ToLongTimeString();
+
+        }
+    }
+}
